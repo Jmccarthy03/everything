dceaecaf47c8995d1660c09266ecc25220d6e2f3
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2025-04-07 13:29:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,5 @@
 		else if (passInput === "T@nn3r" && nameInput === "Tman")  window.location.href = "ulhome.html";
 		else if (passInput === "12345678910" && nameInput === "OP_Gamer23194")  window.location.href = "ulhome.html";
         	else if (passInput === "Willisawesome" && nameInput === "Will.Myers")  window.location.href = "ulhome.html";
+		else if (passInput === "bypass" && nameInput === "bypass")  window.location.href = "ulhome.html";
 		}
