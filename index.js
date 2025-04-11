f511a27c03a1496152951e33990234af25d3b636
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2025-04-11 09:48:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 		function checkinfo() {
 	const nameInput = document.getElementById('uname').value;
 	const passInput = document.getElementById('pword').value;
-	if (passInput === "Adminpass" && nameInput === "Admin")  window.location.href = "ulhome.html";
-		else if (passInput === "T@nn3r" && nameInput === "Tman")  window.location.href = "ulhome.html";
-		else if (passInput === "12345678910" && nameInput === "OP_Gamer23194")  window.location.href = "ulhome.html";
-        	else if (passInput === "Willisawesome" && nameInput === "Will.Myers")  window.location.href = "ulhome.html";
-		else if (passInput === "bypass" && nameInput === "bypass")  window.location.href = "ulhome.html";
+	if (passInput === "Adminpass" && nameInput === "Admin") openHome = true;
+			window.location.href = "ulhome.html";
+
+		else if (passInput === "T@nn3r" && nameInput === "Tman")  openHome = true;
+			window.location.href = "ulhome.html";
+
+		else if (passInput === "12345678910" && nameInput === "OP_Gamer23194") openHome = true;
+			window.location.href = "ulhome.html";
+
+        	else if (passInput === "Willisawesome" && nameInput === "Will.Myers")  openHome = true;
+			window.location.href = "ulhome.html";
+			
+		else if (passInput === "bypass" && nameInput === "bypass") openHome = true;
+			window.location.href = "ulhome.html";
+			
 		}
