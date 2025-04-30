de94431fa4ca56195a8740de368414b2d8957171
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2025-04-30 09:24:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-let openHome = false;
+//let openHome = false;
 		function checkinfo() {
 	const nameInput = document.getElementById('uname').value;
 	const passInput = document.getElementById('pword').value;
 	if (passInput === "Adminpass" && nameInput === "Admin") {
 			window.location.href = "ulhome.html";
-			sessionStorage.setItem("sec, true);
-			openHome = sessionStorage.getItem("sec");
+			//sessionStorage.setItem("sec, true);
+			//openHome = sessionStorage.getItem("sec");
 			}
 		else if (passInput === "T@nn3r" && nameInput === "Tman")  {
 			window.location.href = "ulhome.html";
-			sessionStorage.setItem("sec, true);
-			openHome = sessionStorage.getItem("sec");
+			//sessionStorage.setItem("sec, true);
+			//openHome = sessionStorage.getItem("sec");
 	}
 		else if (passInput === "12345678910" && nameInput === "OP_Gamer23194"){
 			window.location.href = "ulhome.html";
-			sessionStorage.setItem("sec, true);
-			openHome = sessionStorage.getItem("sec");
+			//sessionStorage.setItem("sec, true);
+			//openHome = sessionStorage.getItem("sec");
 		}
         	else if (passInput === "Willisawesome" && nameInput === "Will.Myers") {
 			window.location.href = "ulhome.html";
-			sessionStorage.setItem("sec, true);
-			openHome = sessionStorage.getItem("sec");
+			//sessionStorage.setItem("sec, true);
+			//openHome = sessionStorage.getItem("sec");
 			}
 		else if (passInput === "bypass" && nameInput === "bypass") {
 			window.location.href = "ulhome.html";
-			sessionStorage.setItem("sec, true);
-			openHome = sessionStorage.getItem("sec");
+			//sessionStorage.setItem("sec, true);
+			//openHome = sessionStorage.getItem("sec");
 			}
 		}
 	
 
-	function security() {
-		if (openHome === false) { window.location.href = "index.html"; }
-	}
+	//function security() {
+	//	if (openHome === false) { window.location.href = "index.html"; }
+	//}
