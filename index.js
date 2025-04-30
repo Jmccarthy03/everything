3940ce7df322eaa36404a9b89f9c9b5050f7aea9
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2025-04-30 09:18:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-let openHome = false
+let openHome = false;
 		function checkinfo() {
 	const nameInput = document.getElementById('uname').value;
 	const passInput = document.getElementById('pword').value;
 	if (passInput === "Adminpass" && nameInput === "Admin") {
 			window.location.href = "ulhome.html";
+			sessionStorage.setItem("sec, true);
 			}
 		else if (passInput === "T@nn3r" && nameInput === "Tman")  {
 			window.location.href = "ulhome.html";
-			openHome = true;
+			sessionStorage.setItem("sec, true);
 	}
 		else if (passInput === "12345678910" && nameInput === "OP_Gamer23194"){
 			window.location.href = "ulhome.html";
-			openHome = true;
+			sessionStorage.setItem("sec, true);
 		}
         	else if (passInput === "Willisawesome" && nameInput === "Will.Myers") {
 			window.location.href = "ulhome.html";
-			openHome = true;
+			sessionStorage.setItem("sec, true);
 			}
 		else if (passInput === "bypass" && nameInput === "bypass") {
 			window.location.href = "ulhome.html";
-			openHome = true;
+			sessionStorage.setItem("sec, true);
 			}
 		}
-	
+	let openHome = sessionStorage.getItem("sec");
 
 	function security() {
 		if (openHome === false) { window.location.href = "index.html"; }
