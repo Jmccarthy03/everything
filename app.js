6dca4da918e4ca6bb4ae9896cbd325f476aeca82
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2025-05-13 10:39:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-if (
+<script>
+  // This detects iOS standalone mode
+  if (('standalone' in window.navigator) && window.navigator.standalone) {
+    document.addEventListener('click', function(event) {
+      let target = event.target.closest('a');
+      if (target && target.href && target.host === location.host) {
+        event.preventDefault();
+        window.location = target.href;
+      }
+    }, false);
+  }
+</script>
